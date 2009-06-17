ec17646b13bac7e786bbcf336fc78ac1b0602e72
John Trammell <johntrammell@gmail.com>
2009-06-17 16:17:14 -0500

    removed crud

--- appengine/met/content.py
+++ appengine/met/content.py
@@ -4,6 +4,8 @@
 The exam portion of the Ethics training.
 """
 
+import json
+
 class SceneTemplate(object):
     pass
 
@@ -26,27 +28,4 @@ class Answer(object):
 
 class TestBank(object):
     pass
-
-
-Question(
-    stem="""May you sit on the panel hearing the charge of discrimination?""",
-    prompt="""Please reflect first on the reason we have this conflict of
-    interest statute and how it applies here. Then choose the best answer.
-    """
-
-Yes. You can hear this case because you have a high level of integrity and can
-assure the Chair that you will not let your stock ownership and your
-daughter’s employment influence you in making decisions on this case.
-Maybe, if you sell the stock and determine that your relationship with your
-daughter and her employment will not affect your decisions in the case.
-No, because your financial interest in the stock and your daughter’s
-employment preclude you from hearing the case.
-Yes, if you sell your stock and disown your daughter.
-
-
-
-)
-
-
-
 
