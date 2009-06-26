2de1f060976ae461dede527868ca2d12346351d4
John Trammell <johntrammell@gmail.com>
2009-06-26 14:06:40 -0500

    removed setuptools

--- setup.py
+++ setup.py
@@ -1,33 +1,14 @@
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup, find_packages
-import os
+#!/usr/bin/env python
+
+from distutils.core import setup
 
 setup(
     name='mpls.ethics',
-    version='0.0.1',
-    description="Individual and group reports for the CPA PCP Pretest product",
-    long_description=open("README.txt").read() + "\n" +
-        open(os.path.join("doc", "HISTORY.txt")).read(),
-    classifiers=[
-        "Programming Language :: Python",
-        "Intended Audience :: Education",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Natural Language :: English",
-        "Topic :: Education :: Computer Aided Instruction (CAI)",
-        "Topic :: Education :: Testing",
-    ],
-    keywords='Minneapolis ethics training',
+    version='0.1',
+    description="",
     author='John Trammell',
     author_email='johntrammell@gmail.com',
-    url='http://www.johntrammell.com/',
-    license='GPL',
-    packages = find_packages(exclude=['ez_setup']),
-    namespace_packages=[],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['setuptools',],
-    dependency_links = [],
-    entry_points=""
+    url='http://github.com/trammell/met/tree/master',
+    packages=['app.met'],
 )
 
