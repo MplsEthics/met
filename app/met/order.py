f75d489b28b91a8626cdb9d25b4854e7c19ec180
John Trammell <jotr@voh.(none)>
2009-10-17 21:56:57 -0500

    distinguishing between view order and scenario order

---
+++ app/met/order.py
@@ -0,0 +1,66 @@
+# define the navigation view order
+
+# FIXME
+scenario_order = [
+    'coi1',
+    'coi2',
+    'coi3',
+    'coi4',
+]
+
+view_order = [
+    'main',
+    'instr1',
+    'instr2',
+    'over1',
+    'over2',
+
+    # introduce topic 1: conflict of interest
+    'topic1',
+
+    # conflict of interest part 1
+    'coi1/intro',
+    'coi1/scenario',
+    'coi1/disc1',
+    'coi1/disc2',
+
+    # conflict of interest part 2
+    'coi2/intro',
+    'coi2/scenario',
+    'coi2/disc1',
+    'coi2/disc2',
+    'coi2/disc3',
+
+    # conflict of interest part 3
+    'coi3/intro',
+    'coi3/scenario',
+    'coi3/disc1',
+    'coi3/disc2',
+    'coi3/disc3',
+
+    # conflict of interest part 4
+    'coi4/intro',
+    'coi4/scenario',
+    'coi4/disc1',
+
+    # disclosure of information
+    'doi/intro',
+    'doi/scenario',
+    'doi/disc1',
+
+    # gifts
+    'gifts/intro1',
+    'gifts/intro2',
+    'gifts/scenario',
+    'gifts/disc1',
+
+    # ethics report line & contact info
+    'reportline1',
+    'reportline2',
+    'reportline3',
+
+    'contacts',
+    'congrats',
+    'certificate',
+]
+
