41054d79cff879528812803fea33b4d58d4d4541
John Trammell <jotr@voh.(none)>
2009-10-18 13:30:26 -0500

    fixed nav order

--- app/met/order.py
+++ app/met/order.py
@@ -1,6 +1,4 @@
-# define the navigation view order
-
-# FIXME
+# define the scenario order
 scenario_order = [
     'coi1',
     'coi2',
@@ -10,6 +8,7 @@ scenario_order = [
     'gifts',
 ]
 
+# define the navigation view order
 view_order = [
     'main',
     'instr1',
@@ -17,36 +16,37 @@ view_order = [
     'over1',
     'over2',
 
-    # introduce topic 1: conflict of interest
-    'topic1',
-
     # conflict of interest part 1
-    'coi1/intro',
+    'coi1/intro1',
+    'coi1/intro2',
     'coi1/scenario',
     'coi1/disc1',
     'coi1/disc2',
 
     # conflict of interest part 2
-    'coi2/intro',
+    'coi2/intro1',
     'coi2/scenario',
     'coi2/disc1',
     'coi2/disc2',
     'coi2/disc3',
 
     # conflict of interest part 3
-    'coi3/intro',
+    'coi3/intro1',
     'coi3/scenario',
     'coi3/disc1',
     'coi3/disc2',
     'coi3/disc3',
+    'coi3/disc4',
 
     # conflict of interest part 4
-    'coi4/intro',
+    'coi4/intro1',
     'coi4/scenario',
     'coi4/disc1',
+    'coi4/disc2',
+    'coi4/disc3',
 
     # disclosure of information
-    'doi/intro',
+    'doi/intro1',
     'doi/scenario',
     'doi/disc1',
 
@@ -55,14 +55,16 @@ view_order = [
     'gifts/intro2',
     'gifts/scenario',
     'gifts/disc1',
+    'gifts/disc2',
 
     # ethics report line & contact info
     'reportline1',
     'reportline2',
     'reportline3',
-
-    'contacts',
+    'summary',
     'congrats',
+
+    # certificate email / proof of completion
     'certificate',
 ]
 
