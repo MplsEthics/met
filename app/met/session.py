e59e41730dbc2e878f13686055ea336c5813b580
John Trammell <jotr@voh.(none)>
2009-10-18 23:07:31 -0500

    added code to prepopulate all session containers

--- app/met/session.py
+++ app/met/session.py
@@ -5,6 +5,7 @@
 """
 
 from appengine_utilities import sessions
+from met.order import scenario_order
 
 class Session(sessions.Session):
     """Subclass the appengine_utilities session object to simplify
@@ -13,7 +14,19 @@ class Session(sessions.Session):
 
 class SessionMixin(object):
     def getSession(self):
+
+        # stash the session object in _session
         if not hasattr(self,'session'):
-            self.session = Session()
-        return self.session
+            self._session = Session()
+
+        # make sure all scenario answer arrays are present
+        for s in scenario_order:
+            if not s in self._session:
+                self._session[s] = []
+
+        # make sure the scenario completion dict is present
+        if not 'completed' in self._session:
+            self._session['completed'] = dict()
+
+        return self._session
 
