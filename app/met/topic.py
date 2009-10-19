62b39117dba150b667a8b47208c72dd6fa5dba27
John Trammell <johntrammell@gmail.com>
2009-10-19 13:00:58 -0500

    added topic phrases

--- app/met/topic.py
+++ app/met/topic.py
@@ -6,10 +6,17 @@ Module to handler
 
 def get_topic(scenario_id):
     """This method helps format "Topic" strings consistently."""
-    if 'coi' in scenario_id:
-        return 'Topic 1'
-    if 'doi' in scenario_id:
-        return 'Topic 2'
-    if 'gift' in scenario_id:
-        return 'Topic 3'
+    if scenario_id == 'coi1':
+        return 'Topic One: Conflict of Interest, Part One'
+    if scenario_id == 'coi2':
+        return 'Topic One: Conflict of Interest, Part Two'
+    if scenario_id == 'coi3':
+        return 'Topic One: Conflict of Interest, Part Three'
+    if scenario_id == 'coi4':
+        return 'Topic One: Conflict of Interest, Part Four'
+    if scenario_id == 'doi':
+        return 'Topic Two: Disclosure of Information'
+    if scenario_id == 'gift':
+        return 'Topic Three: Gifts'
+    return None
 
