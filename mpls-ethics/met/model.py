97aa89db6cf6af25c25fe4c1214c315f14beae79
John Trammell <johntrammell@gmail.com>
2009-10-21 17:02:43 -0500

    finished defining Completion model object

--- mpls-ethics/met/model.py
+++ mpls-ethics/met/model.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python2.4
 
-"""
-"""
-
 from google.appengine.ext import db
 
-class Complete(db.Model):
+class Completion(db.Model):
     """This class models an object for recording a learner's completion of the
     ethics training."""
-    name = db.StringProperty()
-    department = db.StringProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
+    name = db.StringProperty(verbose_name='Learner Name')
+    board = db.StringProperty(verbose_name='Learner Board or Commission')
+    date = db.DateTimeProperty(verbose_name='Completion Timestamp',
+        auto_now_add=True)
 
