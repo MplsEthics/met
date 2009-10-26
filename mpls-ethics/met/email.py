6ccc011c1a1d51de5f64d88953bc1652ca845ae4
John Trammell <johntrammell@gmail.com>
2009-10-26 13:26:45 -0500

    cleaned it up, ready for testing

--- mpls-ethics/met/email.py
+++ mpls-ethics/met/email.py
@@ -2,22 +2,15 @@
 
 from google.appengine.api import mail
 
-class ConfirmUserSignup(webapp.RequestHandler):
-    def post(self):
-        user_address = self.request.get("email_address")
-
-        if not mail.is_email_valid(user_address):
-            # prompt user to enter a valid address
-
-        else:
-            confirmation_url = createNewUserConfirmation(self.request)
-            sender_address = "Example.com Support <support@example.com>"
-            subject = "Confirm your registration"
-            body = """
-Thank you for creating an account!  Please confirm your email address
-by clicking on the link below:
-%s
-""" % confirmation_url
-
-            mail.send_mail(sender_address, user_address, subject, body)
+def send_completion(learner_name,learner_board):
+    """Sends the completion email to someone who tracks these things."""
+    sender = 'Ethics Training Completion <completion@mpls-ethics.appspotmail.com>'
+    recipient = 'John Trammell <johntrammell@gmail.com>'
+    subject = "Ethics training completion"
+    body = """
+The following user has completed their online ethics training:
+    name:  %s
+    board: %s
+""" % (learner_name, learner_board)
+    mail.send_mail(sender, recipient, subject, body)
 
