c1ebc593418812fa686abd1c3ea8d69d44bd4a02
John Trammell <johntrammell@gmail.com>
2009-11-09 15:52:29 -0600

    using load_all to load all YAML documents from file

--- bin/check_yaml.py
+++ bin/check_yaml.py
@@ -9,10 +9,9 @@ import os
 import sys
 import yaml
 
-sys.path.append(os.path.join(os.path.dirname(__file__),'../app'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../mpls-ethics'))
 
-from met import content
-
+#from met import content
 #import pdb; pdb.set_trace()
 
 for f in sys.argv[1:]:
@@ -20,7 +19,7 @@ for f in sys.argv[1:]:
     try:
         print "Opening YAML file '%s'..." % f,  # no newline
         fh = open(f)
-        yaml.load(fh)
+        yaml.load_all(fh)
         print " success.\n"
     except yaml.error.YAMLError, e:
         if hasattr(e, 'problem_mark'):
