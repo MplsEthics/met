4846f189f03816034048e7a1132db0bce05f75f2
John Trammell <johntrammell@gmail.com>
2009-11-09 15:26:11 -0600

    removed SessionMixin class; merged into base.py

--- mpls-ethics/met/session.py
+++ mpls-ethics/met/session.py
@@ -1,36 +1,15 @@
 #!/usr/bin/env python
 
 """
-
+This module implements a custom Session class for the Minneapolis Ethics
+Training site.  It uses the GAEUtilities sessions class as its basis (see
+http://code.google.com/p/gaeutilities/).
 """
 
 from appengine_utilities import sessions
-from met.order import scenario_order
 
 class Session(sessions.Session):
     """Subclass the appengine_utilities session object to simplify
     extension."""
     pass
-
-class SessionMixin(object):
-    def getSession(self):
-
-        # stash the session object in _session
-        if not hasattr(self,'session'):
-            #self._session = Session(writer="cookie")
-            self._session = Session()
-
-        # make sure all scenario answer arrays are present
-        for s in scenario_order:
-            if not s in self._session:
-                self._session[s] = []
-
-        # make sure the scenario completion dict is present
-        if not 'completed' in self._session:
-            self._session['completed'] = dict()
-
-        return self._session
-
-    def saveSession(self):
-        """Force a write."""
 
