672d5cc073fb766d95b648a9e368f330f0cb18ed
John Trammell <johntrammell@gmail.com>
2009-11-09 15:29:15 -0600

    added learner data handling and checking

--- mpls-ethics/met/views/learner.py
+++ mpls-ethics/met/views/learner.py
@@ -7,14 +7,15 @@ from met.email import send_completion
 from met.model import Completion
 from met.views import base
 
-class Learner(base.BaseView, session.SessionMixin, order.OrderMixin):
+class Learner(base.SecureView):
 
     def get(self):
         path = self.viewpath(append='learner.djt')
         session = self.getSession()
         show_prevnext = True
         boards = boards_
-        learner_error = self.request.params.get('learner_error',None)
+        learner_error = session.get('learner_error',False)
+        logging.info('''learner_error: "%s"''' % learner_error)
         self.response.out.write(template.render(path,locals()))
 
     def post(self):
@@ -23,17 +24,21 @@ class Learner(base.BaseView, session.SessionMixin, order.OrderMixin):
 
         session = self.getSession()
 
-        # ensure that all scenarios have been completed FIXME
-        # self.assert_scenario_order(scenario_id,session)
+        # ensure that all scenarios have been completed
+        self.assert_all_scenarios_completed()
 
         # update the session as needed based on the answer
-        learner_name = self.request.params.get('learner_name',None)
-        learner_board = self.request.params.get('learner_board',None)
+        learner_name = self.request.params.get('learner_name',"")
+        learner_board = self.request.params.get('learner_board',"")
+
+        #logging.info('''learner_name: "%s"''' % learner_name)
+        #logging.info('''learner_board: "%s"''' % learner_board)
 
         # bail if we don't have both name and board
-        if learner_name is None or learner_board is None:
+        if len(learner_name) == 0 or len(learner_board) == 0:
             session["learner_error"] = True
             self.redirect('/learner')
+            return
 
         # persist the learner name and board in the session
         session["learner_name"] = learner_name
@@ -53,5 +58,6 @@ class Learner(base.BaseView, session.SessionMixin, order.OrderMixin):
             pass
 
         # redirect to the certificate view
+        logging.info('redirecting to /certificate')
         self.redirect('/certificate')
 
