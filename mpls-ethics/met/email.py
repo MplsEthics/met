87848577935de9cfe5c0843d044beb529c053e78
John Trammell <jotr@voh.(none)>
2009-11-13 23:09:01 -0600

    added JT as BCC

--- mpls-ethics/met/email.py
+++ mpls-ethics/met/email.py
@@ -5,12 +5,13 @@ from google.appengine.api import mail
 def send_completion(learner_name,learner_board):
     """Sends the completion email to someone who tracks these things."""
     sender = 'Ethics Training Completion <completion@mpls-ethics.appspotmail.com>'
-    recipient = 'John Trammell <johntrammell@gmail.com>'
+    recipient = 'Ethics Education <ethicseducation@ci.minneapolis.mn.us>'
+    jt = 'John Trammell <johntrammell@gmail.com>'
     subject = "Ethics training completion"
     body = """
 The following user has completed their online ethics training:
     name:  %s
     board: %s
 """ % (learner_name, learner_board)
-    mail.send_mail(sender, recipient, subject, body)
+    mail.send_mail(sender, recipient, subject, body, bcc=jt),
 
