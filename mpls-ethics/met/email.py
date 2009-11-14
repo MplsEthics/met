218ff97adbf31539687135ec78aa803f54053655
John Trammell <jotr@voh.(none)>
2009-11-13 23:31:51 -0600

    using EmailMessage api

--- mpls-ethics/met/email.py
+++ mpls-ethics/met/email.py
@@ -4,14 +4,20 @@ from google.appengine.api import mail
 
 def send_completion(learner_name,learner_board):
     """Sends the completion email to someone who tracks these things."""
-    sender = 'Ethics Training Completion <completion@mpls-ethics.appspotmail.com>'
-    recipient = 'Ethics Education <ethicseducation@ci.minneapolis.mn.us>'
-    jt = 'John Trammell <johntrammell@gmail.com>'
-    subject = "Ethics training completion"
-    body = """
+    msg = mail.EmailMessage()
+
+    # send the message to 'ethics education'
+    msg.sender = 'Ethics Training Completion <completion@mpls-ethics.appspotmail.com>'
+    msg.to = 'Ethics Education <ethicseducation@ci.minneapolis.mn.us>'
+    msg.subject = "Ethics training completion"
+    msg.body = """
 The following user has completed their online ethics training:
     name:  %s
     board: %s
 """ % (learner_name, learner_board)
-    mail.send_mail(sender, recipient, subject, body, bcc=jt)
+    msg.send()
+
+    # send a copy to me
+    msg.to = 'John Trammell <johntrammell@gmail.com>'
+    msg.send()
 
