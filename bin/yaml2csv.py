0cd9e046448bd87b7e18433a092facf2a033d952
John Trammell <johntrammell@gmail.com>
2010-06-14 11:08:42 -0500

    generating good CSV now

--- bin/yaml2csv.py
+++ bin/yaml2csv.py
@@ -8,6 +8,7 @@ import os
 import sys
 import yaml
 import pprint
+import csv
 
 content_ = os.path.join(os.path.dirname(__file__),'../lib/content')
 sys.path.append(content_)
@@ -16,11 +17,35 @@ sys.path.append(content_)
 import schema
 #import pdb; pdb.set_trace()
 
-for f in sys.argv[1:]:
-    print "Opening YAML file '%s'..." % f,  # no newline
-    fh = open(f)
-    objects = [x for x in yaml.load_all(fh)]
-    import pdb; pdb.set_trace()
+# scenarios
+sw = csv.DictWriter(
+    open('scenario.csv','w'),
+    ['id','name','scenario','question','prompt'],
+    extrasaction='ignore',
+)
 
-    pprint.pprint(objects)
+# answers
+aw = csv.DictWriter(
+    open('answers.csv','w'),
+    ['id','answer','is_correct','response'],
+    extrasaction='ignore',
+)
+
+def to_utf8(dict_):
+    d = dict(dict_)
+    for key, value in d.items():
+        if isinstance(value,basestring):
+            d[key] = value.encode("utf-8")
+    return d
+
+if __name__ == '__main__':
+
+    for f in sys.argv[1:]:
+        print "...'%s'" % f
+        fh = open(f)
+        objects = [x for x in yaml.load_all(fh)]
+        scenario = objects[0]
+        sw.writerow(to_utf8(scenario.__dict__))
+        for ans in scenario.answers:
+            aw.writerow(to_utf8(ans.__dict__))
 
