79df30d668398bcff2c9e27cae37fc7d7dac9e78
John Trammell <johntrammell@gmail.com>
2010-06-15 13:07:08 -0500

    added __init__ code

--- mpls-ethics/met/session.py
+++ mpls-ethics/met/session.py
@@ -1,15 +1,26 @@
-#!/usr/bin/env python
-
 """
 This module implements a custom Session class for the Minneapolis Ethics
-Training site.  It uses the GAEUtilities sessions class as its basis (see
-http://code.google.com/p/gaeutilities/).
+Training site.  It uses the Appengine Utilities ("GAEUtilities") sessions
+class as its basis (see http://code.google.com/p/gaeutilities/).
 """
 
-from appengine_utilities import sessions
+from appengine_utilities.sessions import Session as GAESession
+from met.order import scenario_order
 
-class Session(sessions.Session):
+class Session(GAESession):
     """Subclass the appengine_utilities session object to simplify
     extension."""
-    pass
+
+    def __init__(self, *args, **kwargs):
+        """Initialize session attributes for the ethics training."""
+
+        # call parent init
+        super(Session,self).__init__(*args, **kwargs)
+
+        # init scenario answer array
+        for so in scenario_order:
+            self.setdefault(so,[])
+
+        # init scenario completion dict
+        self.setdefault('completed',dict())
 
