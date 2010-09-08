339bf32aa9aab98e8df5c78daf378628b2902760
John Trammell <johntrammell@gmail.com>
2010-09-08 16:09:45 -0500

    CSV is generated with header

--- util/bulkloader/bin/yaml2csv.py
+++ util/bulkloader/bin/yaml2csv.py
@@ -10,7 +10,7 @@ import yaml
 import pprint
 import csv
 
-content_ = os.path.join(os.path.dirname(__file__),'../lib')
+content_ = os.path.join(os.path.dirname(__file__), '../lib')
 sys.path.append(content_)
 
 #from met.content import Scenario
@@ -18,27 +18,36 @@ import schema
 #import pdb; pdb.set_trace()
 
 # scenario writer
-sw = csv.DictWriter(
-    open('scenario.csv','w'),
-    ['id','name','scenario','question','prompt'],
-    extrasaction='ignore',
-)
+sw_cols = ['id', 'name', 'scenario', 'question', 'prompt']
+sw = csv.DictWriter(open('scenario.csv', 'w'),
+                    sw_cols,
+                    extrasaction='ignore')
 
 # answer writer
-aw = csv.DictWriter(
-    open('answers.csv','w'),
-    ['id','scenario','answer','is_correct','response'],
-    extrasaction='ignore',
-)
+aw_cols = ['id', 'scenario', 'answer', 'is_correct', 'response']
+aw = csv.DictWriter(open('answers.csv', 'w'),
+                    aw_cols,
+                    extrasaction='ignore',)
+
 
 def to_utf8(dict_):
     d = dict(dict_)
     for key, value in d.items():
-        if isinstance(value,basestring):
+        if isinstance(value, basestring):
             d[key] = value.encode("utf-8")
     return d
 
+
+def self_dict(list_):
+    """Convert list to dict."""
+    items = [(x, x) for x in list_]
+    return dict(items)
+
+
 if __name__ == '__main__':
+    # write the header columns
+    sw.writerow(to_utf8(self_dict(sw_cols)))
+    aw.writerow(to_utf8(self_dict(aw_cols)))
 
     for f in sys.argv[1:]:
         print "converting '%s' to CSV..." % f
@@ -49,4 +58,3 @@ if __name__ == '__main__':
         for ans in scenario.answers:
             ans.scenario = scenario.id
             aw.writerow(to_utf8(ans.__dict__))
-
