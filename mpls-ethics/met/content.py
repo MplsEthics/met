956c5581449ee37d585d9477e5a696625d402aa8
John Trammell <johntrammell@gmail.com>
2010-09-08 21:09:00 -0500

    removed get_scenario()

--- mpls-ethics/met/content.py
+++ mpls-ethics/met/content.py
@@ -4,12 +4,6 @@ Classes and other utilities related to dynamic question content.
 
 from met.model import Scenario
 
-def get_scenario(scenario_id):
-    """Finds the scenario corresponding to the indicated ID.  We return a
-    copy, because GAE persists the 'testbank' global, and we don't want any
-    scary action-at-a-distance.  That might take hours to debug!"""
-    return Scenario.gql("WHERE id = :1",scenario_id).get()
-
 
 class InvalidAnswerError(Exception):
 
@@ -24,21 +18,25 @@ class LearnerScenario(object):
     """Objects of this class handle interactions between scenarios and learner
     sessions."""
 
-    def __init__(self,scenario_id,session):
+    prompt = "FIXME, maybe use getattr?"
+
+    def __init__(self, scenario_id, session):
         """Construct the hybrid object."""
         self.scenario_id = scenario_id
         self.session = session
-        self.scenario = Scenario.gql("WHERE id = :1", scenario_id).get()
+        self.scenario = Scenario.get_by_key_name(scenario_id)
 
     def is_completed(self):
         """Returns True if this learner has completed this scenario."""
+        session = self.session
+        scenario_id = self.scenario_id
         try:
-            return self.session['completed'].get(self.scenario_id,False)
+            return session['completed'].get(scenario_id,False)
         except:
             return False
 
     def learner_answers(self):
-        return self.session.get(self.scenario_id,[])
+        return self.session.get(self.scenario_id, [])
 
     def last_answer(self):
         """Returns the learner's last answer to this scenario, or None if no
@@ -96,35 +94,10 @@ class LearnerScenario(object):
             else:
                 setattr(answer,"class","answer incorrect")
 
-
-#    def answer_dict(self):
-#        """Returns a dictionary of answers?"""
-#
-#    # if the scenario has been completed, disable all user inputs, and set the
-#    # user response to the correct value
-#    if scenario.completed:
-#        for a in scenario.answers:
-#            setattr(a,"disabled",True)
-#            if a.correct:
-#                setattr(a,"class","answer correct")
-#                setattr(scenario,"response",a.response)
-#            else:
-#                setattr(a,"class","answer incorrect")
-#
-#    # since the scenario has *not* been completed, make sure the response is
-#    # for the **most recent** answer
-#    else:
-#        scenario.answer_dict[last_answer].checked = True
-#        scenario.response = scenario.answer_dict[last_answer].response
-#
-#    return scenario
-#
-
     def is_valid_answer(self,answer_id):
         """Returns True if answer_id is valid for this scenario."""
         valid_answer_ids = scenario.answer_dict.keys()
         return answer_id in valid_answer_ids
-
 
     def record_answer(self,answer_id):
         """Record this answer and responds accordingly."""
@@ -144,4 +117,3 @@ class LearnerScenario(object):
             completed = self.session["completed"]
             completed[scenario_id] = datetime.now().isoformat()
             self.session["completed"] = completed
-
