9c736c5c2d1a5cd2004bf4b5a0dd9037c03a8c31
John Trammell <johntrammell@gmail.com>
2010-09-09 17:04:42 -0500

    moving session manip into state class

--- mpls-ethics/met/views/learner.py
+++ mpls-ethics/met/views/learner.py
@@ -1,33 +1,52 @@
 import logging
 from google.appengine.ext.webapp import template
-from met.boards import boards as boards_
+from met.boards import boards
 from met.email import send_completion
 from met.model import Completion
 from met.views.base import SecureView
+from met.session import LearnerState
 
 
 class Learner(SecureView):
 
+    @alldone
     def get(self):
-        # ensure that all scenarios have been completed
-        self.assert_all_scenarios_completed()
-
         path = self.viewpath(append='learner.djt')
-        session = self.get_session()
-        show_prevnext = True
-        boards = boards_
         learner_error = session.get('learner_error', False)
-        logging.info('''learner_error: "%s"''' % learner_error)
-        self.response.out.write(template.render(path, locals()))
+        context = dict(show_prevnext=True,
+                       boards=boards,
+                       learner_error=learner_error,
+                       session=state.session_fmt())
+        output = template.render(path, context)
+        self.response.out.write(output)
 
+    @alldone
     def post(self):
-        """Process the learner data submission, then redirect to the
-        'certificate' view."""
+        """Persist the learner data in the GAE datastore and in the session,
+        then send the email and redirect to the 'certificate' view."""
 
-        session = self.get_session()
+        state = LearnerState()
 
-        # ensure that all scenarios have been completed
-        self.assert_all_scenarios_completed()
+        # persist the learner data
+        try:
+            name = self.request.params.get('learner_name', "")
+            board_id = self.request.params.get('learner_board_id', "")
+            state.persist_learner(name, board_id)
+        except InvalidLearnerException:
+            session["learner_error"] = True
+            self.redirect('/learner')
+            return
+
+        # send the completion email
+        try:
+            send_completion(learner_name, learner_board)
+        except:
+            pass
+
+        # redirect to the certificate view
+        self.redirect('/certificate')
+
+# FIXME: cleanup
 
         # update the session as needed based on the answer
 
