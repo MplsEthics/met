ba195f962423c7e04d34efd8220db37907f4bc9d
John Trammell <johntrammell@gmail.com>
2010-09-09 17:07:06 -0500

    cleaning up base view proliferation

--- mpls-ethics/met/views/base.py
+++ mpls-ethics/met/views/base.py
@@ -3,6 +3,7 @@ from google.appengine.ext import webapp
 from met.order import scenario_order, view_order
 from met.session import Session
 
+
 class BaseView(webapp.RequestHandler):
     """Base class for all MET (Minneapolis Ethics Training) view classes."""
 
@@ -14,7 +15,7 @@ class BaseView(webapp.RequestHandler):
         """Returns the path to the 'main' view template."""
         return self.viewpath('main.djt')
 
-    def viewpath(self,append=None):
+    def viewpath(self, append=None):
         """Construct a view path."""
         if append:
             return os.path.join(self.view_dir, './' + append)
@@ -30,7 +31,7 @@ class BaseView(webapp.RequestHandler):
         try:
             i = self.view_index()
             if i < len(view_order):
-                return view_order[i+1]
+                return view_order[i + 1]
         except:
             return None
 
@@ -39,7 +40,7 @@ class BaseView(webapp.RequestHandler):
         try:
             i = self.view_index()
             if i > 0:
-                return view_order[i-1]
+                return view_order[i - 1]
         except:
             return None
 
@@ -61,63 +62,3 @@ class BaseView(webapp.RequestHandler):
     def post(self):
         """Default POST action is to redirect to GET."""
         self.redirect(self.request.path)
-
-
-class SessionView(BaseView):
-    """Just like BaseView, with the addition of method get_session(), which
-    allows access to the session."""
-
-    def get_session(self):
-        """Cache and return an initialized session object."""
-        if getattr(self,'_session',None) is None:
-            self._session = Session()
-        return self._session
-
-
-class SecureView(SessionView):
-    """This view class extends the session view with features for ensuring
-    that learners are not accessing views out of order."""
-
-    def assert_all_scenarios_completed(self):
-        if not self.all_scenarios_completed():
-            incomplete = self.first_incomplete_scenario()
-            self.redirect("/%s/intro1" % incomplete)
-
-    def assert_scenario_order(self,scenario_id):
-        """If the learner is trying to access this scenario out of order,
-        redirect to the first incomplete scenario."""
-        # FIXME: do I need compdict?
-        compdict = self.get_session()['completed']
-        if not self.prereqs_completed(scenario_id):
-            incomplete = self.first_incomplete_scenario()
-            self.redirect("/%s/intro1" % incomplete)
-
-    def prereqs_completed(self,scenario_id):
-        """Returns True if all the scenarios before to 'scenario_id' have been
-        completed, as indicated by their status in 'compdict'.  Returns False
-        otherwise.  This function can be used to determine if the user is
-        trying to complete the scenarios out of order."""
-        compdict = self.get_session()["completed"]
-        assert scenario_id in scenario_order, 'test scenario must be known'
-        k = scenario_order.index(scenario_id)
-        for sid in scenario_order[0:k]:
-            if sid not in compdict:
-                return False
-        return True
-
-    def all_scenarios_completed(self):
-        """Returns True if all the scenarios are in the dict that records
-        scenario completiions ('compdict').  Returns False otherwise."""
-        compdict = self.get_session()["completed"]
-        for sid in scenario_order:
-            if sid not in compdict:
-                return False
-        return True
-
-    def first_incomplete_scenario(self):
-        compdict = self.get_session()["completed"]
-        for sid in scenario_order:
-            if sid not in compdict:
-                return sid
-        return None
-
