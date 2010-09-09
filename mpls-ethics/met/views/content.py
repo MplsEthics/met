e58f5ac950b67d511896dbd2daad097b5e6815c5
John Trammell <johntrammell@gmail.com>
2010-09-09 09:33:20 -0500

    cleanup

--- mpls-ethics/met/views/content.py
+++ mpls-ethics/met/views/content.py
@@ -2,16 +2,17 @@ from google.appengine.ext import webapp
 from met.views.base import BaseView
 from met.model import Scenario
 
-class Content(BaseView):
 
-    def get(self,scenario_id,view):
+class Content(BaseView):
+    """Shows any page containing scenario content."""
+
+    def get(self, scenario_id, view):
         template = "%s/%s.djt" % (scenario_id, view)
         path = self.viewpath(append=template)
         context = dict(next=self.next(),
                        previous=self.previous(),
                        s=Scenario.get_by_key_name(scenario_id),
                        show_prevnext=True)
-        self.response.out.write(webapp.template.render(path,context))
+        self.response.out.write(webapp.template.render(path, context))
 
     post = get
-
