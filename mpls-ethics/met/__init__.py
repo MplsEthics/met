9c1a68937fa581f69cbaa5ea88dce31b8517507d
John Trammell <johntrammell@gmail.com>
2010-09-09 08:30:16 -0500

    moved debug code into met

---
+++ mpls-ethics/met/__init__.py
@@ -0,0 +1,10 @@
+import os
+import pdb
+import sys
+
+content_dir = os.path.join(os.path.dirname(__file__), '../../content')
+
+def debug():
+    for attr in ('stdin', 'stdout', 'stderr'):
+        setattr(sys, attr, getattr(sys, '__%s__' % attr))
+    pdb.set_trace()
