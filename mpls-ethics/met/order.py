b3e51ba34e2ff1f8f043d8b090d348f530491630
John Trammell <johntrammell@gmail.com>
2010-09-08 21:05:29 -0500

    updated to reflect view rename

--- mpls-ethics/met/order.py
+++ mpls-ethics/met/order.py
@@ -19,20 +19,20 @@ view_order = [
     # conflict of interest part 1
     'coi1/intro1',
     'coi1/intro2',
-    'coi1/scenario',
+    'coi1/question',
     'coi1/disc1',
     'coi1/disc2',
 
     # conflict of interest part 2
     'coi2/intro1',
-    'coi2/scenario',
+    'coi2/question',
     'coi2/disc1',
     'coi2/disc2',
     'coi2/disc3',
 
     # conflict of interest part 3
     'coi3/intro1',
-    'coi3/scenario',
+    'coi3/question',
     'coi3/disc1',
     'coi3/disc2',
     'coi3/disc3',
@@ -40,20 +40,20 @@ view_order = [
 
     # conflict of interest part 4
     'coi4/intro1',
-    'coi4/scenario',
+    'coi4/question',
     'coi4/disc1',
     'coi4/disc2',
     'coi4/disc3',
 
     # disclosure of information
     'doi/intro1',
-    'doi/scenario',
+    'doi/question',
     'doi/disc1',
 
     # gifts
     'gifts/intro1',
     'gifts/intro2',
-    'gifts/scenario',
+    'gifts/question',
     'gifts/disc1',
     'gifts/disc2',
 
