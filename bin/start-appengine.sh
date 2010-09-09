f750c6882a273b82cc2932525b101a4a9d67bb61
John Trammell <johntrammell@gmail.com>
2010-09-08 20:47:56 -0500

    fixed local address

--- bin/start-appengine.sh
+++ bin/start-appengine.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 APPSERVER=$APPENGINE/dev_appserver.py
+ADDRESS=127.0.0.1
 
-if [ "$(hostname)" == 'voh' ]; then
-    ADDRESS=127.0.0.1
-else
-    echo $(hostname)
-    ADDRESS=10.1.6.111
-fi
+echo $PYTHON25 $APPSERVER --port=8765 --address=$ADDRESS mpls-ethics/
 
 $PYTHON25 $APPSERVER --port=8765 --address=$ADDRESS mpls-ethics/
