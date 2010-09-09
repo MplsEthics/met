eb3f8ea94b534b17ba122646c4928f3f65c25811
John Trammell <johntrammell@gmail.com>
2010-09-09 16:45:20 -0500

    fixed ordered, alldone decorators

--- mpls-ethics/met/decorators.py
+++ mpls-ethics/met/decorators.py
@@ -4,21 +4,43 @@
 from met.session import LearnerState
 
 
-class Ordered(object):
+def ordered(func):
     """If the learner is trying to access the questions out of order, redirect
     to the first incomplete scenario."""
 
-    def __init__(self, f):
-        self.func = f
-
-    def __call__(self, scenario_id, *argv, **kwargs):
-        # if the prerequisites for this scenario have been completed, then
-        # display the view, otherwise redirect to the start of the first
-        # incomplete scenario
+    def wrapper(self, scenario_id, *args, **kwargs):
+        """
+        If the prerequisites for this scenario have been completed, then
+        display the view, otherwise redirect to the start of the first
+        incomplete scenario.
+        """
         state = LearnerState()
         if state.completed_prerequisites(scenario_id):
-            return self.func(scenario_id, *argv, **kwargs)
+            return func(self, scenario_id, *argv, **kwargs)
         else:
             scenario_id = state.first_incomplete_scenario()
             self.redirect("/%s/intro1" % scenario_id)
             return
+
+    return wrapper
+
+
+def alldone(func):
+    """If the learner is trying to access a view before having completed all
+    scenarios, redirect to the first incomplete scenario."""
+
+    def wrapper(self, scenario_id, *args, **kwargs):
+        """
+        If the prerequisites for this scenario have been completed, then
+        display the view, otherwise redirect to the start of the first
+        incomplete scenario.
+        """
+        state = LearnerState()
+        if state.completed_all():
+            return func(self, scenario_id, *argv, **kwargs)
+        else:
+            scenario_id = state.first_incomplete_scenario()
+            self.redirect("/%s/intro1" % scenario_id)
+            return
+
+    return wrapper
