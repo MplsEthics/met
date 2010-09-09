80aa2f69ae9e8c87aafec306e0b81ff2fc9e2a5f
John Trammell <johntrammell@gmail.com>
2010-09-09 14:24:36 -0500

    cleanup; moved state code to LearnerState

--- mpls-ethics/met/views/response.py
+++ mpls-ethics/met/views/response.py
@@ -7,37 +7,23 @@ from met.views.base import SessionView
 class Response(SessionView):
 
     def get(self, scenario_id):
-        session = self.get_session()
         state = LearnerState()
-        answer_id = self.last_answer_id(scenario_id)
-        answer = Answer.get_by_key_name(answer_id)
+        answer_id = state.last_answer_id(scenario_id)
+        response = Answer.get_by_key_name(answer_id).response
         path = self.viewpath(append='response.djt')
+
         if state.is_completed(scenario_id):
             link_next = "/%s/disc1" % scenario_id
         else:
             link_next = "/%s/question" % scenario_id
-        context = dict(session=session,
+
+        context = dict(session=state.session_fmt(),
                        next=self.next(),
                        previous=self.previous(),
                        s=state,
                        show_prevnext=False,
-                       correct=state.is_completed(),
-                       response=answer.response,
+                       correct=state.is_completed(scenario_id),
+                       response=response,
                        link_next=link_next)
         output = webapp.template.render(path, context)
         self.response.out.write(output)
-
-    def last_answer_id(self, scenario_id):
-        try:
-            return self.get_session()[scenario_id][-1]
-        except:
-            return None
-
-    def learner_response(self, scenario, answer_id):
-        """Returns a string containing the response we want to give to the
-        learner."""
-        answer = scenario.answer_dict.get(answer_id, "")
-        if answer is not None:
-            return answer.response
-        else:
-            return None
