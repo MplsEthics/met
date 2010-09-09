b9d38e599d32d5bbc0359feb709f5c8f0ed4b6cc
John Trammell <johntrammell@gmail.com>
2010-09-09 10:37:39 -0500

    added scenario exception

--- mpls-ethics/met/exceptions.py
+++ mpls-ethics/met/exceptions.py
@@ -3,6 +3,15 @@ Custom exceptions for MET product.
 """
 
 
+class InvalidScenarioException(Exception):
+
+    def __init__(self, value):
+        self.parameter = value
+
+    def __str__(self):
+        return repr(self.parameter)
+
+
 class InvalidAnswerException(Exception):
 
     def __init__(self, value):
