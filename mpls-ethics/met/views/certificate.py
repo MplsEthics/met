44ff514e96820e7a5152ab13343490fc22e6ebf6
John Trammell <johntrammell@gmail.com>
2010-09-09 17:04:52 -0500

    alldone

--- mpls-ethics/met/views/certificate.py
+++ mpls-ethics/met/views/certificate.py
@@ -1,18 +1,21 @@
-import datetime
+from datetime import datetime
 from google.appengine.ext import webapp
-
+from met.decorators import ordered
 from met.views.base import SessionView
-from met.version import version as VERSION
+from met.version import version
+
 
 class Certificate(SessionView):
     """View class for the certificate."""
 
+    @alldone
     def get(self, *argv):
         path = self.viewpath(append='certificate.djt')
-        show_prevnext = False
-        version = VERSION
-        now = datetime.datetime.now()
-        session = self.get_session()
-        learner_name = session.get('learner_name',None)
-        learner_board = session.get('learner_board',None)
-        self.response.out.write(webapp.template.render(path, locals()))
+        context = dict(show_prevnext=False,
+                       version=version,
+                       now=datetime.now(),
+                       session=state.session_fmt(),
+                       learner_name=session.get('learner_name', None),
+                       learner_board=session.get('learner_board', None))
+        output = webapp.template.render(path, context)
+        self.response.out.write(output)
