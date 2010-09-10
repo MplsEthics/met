57c42fe9895bec5eaa9bdee306a29ab151d16b27
John Trammell <johntrammell@gmail.com>
2010-09-10 11:13:36 -0500

    fixed cookie set

--- mpls-ethics/met/views/main.py
+++ mpls-ethics/met/views/main.py
@@ -14,9 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Mpls-ethics.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 from google.appengine.ext.webapp import template
 from met.views.base import BaseView
 from met.session import LearnerState
+from time import gmtime, strftime, time
 
 
 class Main(BaseView):
@@ -25,6 +27,18 @@ class Main(BaseView):
         path = self.viewpath(append='main.djt')
         state = LearnerState()
         state.update_timestamp()
+
+        # if there are no cookies, set one and redirect to /cookies
+        if not '_mplsethics' in self.request.cookies:
+            logging.info('performing missing cookie redirect')
+            headers = self.response.headers
+            soon = gmtime(time() + 300)
+            expires = strftime("%a, %d %b %Y %H:%M:%S +0000", soon)
+            headers.add_header('Set-Cookie',
+                               '_mplsethics=1; expires=%s' % expires)
+            self.redirect('/cookies')
+            return
+
         context = dict(next='instr1',
                        show_prevnext=True,
                        show_about=True,
