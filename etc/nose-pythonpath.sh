af13d63ed2366ad5a1683a7a25a1cf39ac6731f3
John Trammell <johntrammell@gmail.com>
2010-09-10 13:24:02 -0500

    still broken

--- etc/nose-pythonpath.sh
+++ etc/nose-pythonpath.sh
@@ -1,10 +1,10 @@
-GAEBASE="/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine"
+GAEBASE="/usr/local/google_appengine"
 
-PYTHONPATH=$PYTHONPATH:$GAEBASE/
-PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/
-PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/django/
-PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/ipaddr/
-PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/webob/
+PYTHONPATH=$GAEBASE
+PYTHONPATH=$PYTHONPATH:$GAEBASE/lib
+PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/django
+PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/ipaddr
+PYTHONPATH=$PYTHONPATH:$GAEBASE/lib/webob
 PYTHONPATH=$PYTHONPATH:/Users/johntrammell/work/github/met/mpls-ethics
 
 export PYTHONPATH
