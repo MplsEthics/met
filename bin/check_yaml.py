e321a5be28171a3a1bc820d648ac1133a453c247
John Trammell <johntrammell@gmail.com>
2010-09-10 08:23:38 -0500

    cleanup

--- bin/check_yaml.py
+++ bin/check_yaml.py
@@ -4,26 +4,19 @@
 Script to check formatting of YAML files.
 """
 
-import os
-#import re
 import sys
 import yaml
 
-sys.path.append(os.path.join(os.path.dirname(__file__),'../mpls-ethics'))
-
-#from met import content
-#import pdb; pdb.set_trace()
 
 for f in sys.argv[1:]:
     # pylint: disable-msg=E1101
     try:
-        print "Opening YAML file '%s'..." % f,  # no newline
+        print "Checking YAML file '%s'..." % f,  # no newline
         fh = open(f)
         yaml.load_all(fh)
-        print " success.\n"
+        print " success."
     except yaml.error.YAMLError, e:
         if hasattr(e, 'problem_mark'):
             m = e.problem_mark
             x = (f, m.line + 1, m.column + 1)
             print "Error in file '%s', line %s, column %s" % x
-
